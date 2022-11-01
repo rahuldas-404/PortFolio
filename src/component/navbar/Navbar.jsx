e5b9df3e1d168708ccca3374656b18d32d9832df
Rahul <you@example.com>
2022-11-01 12:39:08 +0530

    Navbar created

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+        <div className='n-left'>
+            <div className="name">Rahul Kumar Das</div>
+            <span>Toggle</span>
+        </div>
+        <div className='n-right'>
+            <div className="list">
+                <ul style={{listStyleType: 'none'}}> 
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Experience</li>
+                    <li>Portfolio</li>
+                    <li>Testimonials</li>
+                </ul>
+            </div>
+            <button className="button">
+                Contact us
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
